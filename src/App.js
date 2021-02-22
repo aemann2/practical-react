ca98120697c5f5d9cace620e565aff911347685f
Adam Mann <aemann2@gmail.com>
2021-02-22 11:56:04 -0800

    finished part 4

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import Header from './components/layout/header';
 function App() {
   return (
     <div className='App'>
-      <Header />
+      {/* passing "title" as a prop to Header...I don't have to wrap it in curly braces because it's a string */}
+      <Header
+        title='Hello from App'
+        time={new Date().toISOString().slice(0, 10)}
+      />
+      <body>
+        <p>This is body text</p>
+      </body>
     </div>
   );
 }
--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import logo from '../../logo.svg';
+import logo from '../../../src/logo.svg';
 
-const Header = () => {
+const Header = (props) => {
+  // destructuring the props passed in from App.js so I don't have to use dot-notation each time I want to use the objects in the prop
+  const { title, time } = props;
+
   return (
     <header className='App-header'>
       <img src={logo} className='App-logo' alt='logo' />
-      <p>Hi</p>
+      <p>{title}</p>
+      <p>{time}</p>
       <a
         className='App-link'
         href='https://reactjs.org'
