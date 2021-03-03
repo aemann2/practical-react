7b7f70982bbdb686a99fe9f80455db8d3243109c
Adam Mann <aemann2@gmail.com>
2021-03-03 11:17:09 -0800

    finished part 10

--- src/components/MyForm.js
+++ src/components/MyForm.js
@@ -1,35 +1,66 @@
 import React, { useState } from 'react';
 
 function MyForm() {
-  const [name, setName] = useState('');
-  const [favoritePet, setPet] = useState('');
-  const [rememberMe, setRememberMe] = useState(false);
+  // const [name, setName] = useState('');
+  // const [favoritePet, setPet] = useState('');
+  // const [rememberMe, setRememberMe] = useState(false);
 
-  // setting the event handler for the input onChange
+  // to set many states at once, we can do this
+  const [state, setState] = useState({
+    name: '',
+    favoritePet: '',
+    rememberMe: false,
+  });
+
+  // setting the event handlers for all of the fields at once
   const handleChange = (e) => {
-    setName(e.target.value);
+    // setting up a ternary to handle the checkbox event
+    const value =
+      e.target.type === 'checkbox' ? e.target.checked : e.target.value;
+    setState({
+      ...state,
+      // e.target.name is taken from the name attribute on the form fields
+      [e.target.name]: value,
+    });
   };
 
-  const handleChangePet = (e) => {
-    setPet(e.target.value);
-  };
+  // we can actually consolidate almost all these handlers into one...see above
 
-  const handleCheck = () => {
-    setRememberMe(!rememberMe);
-  };
+  // const handleChangeName = (e) => {
+  //   setName(e.target.value);
+  // };
 
-  const handleSubmit = () => {
-    console.log(name, favoritePet, rememberMe);
+  // const handleChangePet = (e) => {
+  //   setPet(e.target.value);
+  // };
+
+  // const handleCheck = () => {
+  //   setRememberMe(!rememberMe);
+  // };
+
+  const handleSubmit = (e) => {
+    // preventing the form from submitting
+    e.preventDefault();
+    console.log(state.name, state.favoritePet, state.rememberMe);
   };
 
   return (
-    <div>
+    <form onSubmit={handleSubmit}>
       {/* passing the state value over as a prop */}
-      <input value={name} onChange={handleChange} />
-      <textarea value={favoritePet} onChange={handleChangePet} />
-      <input type='checkbox' checked={rememberMe} onChange={handleCheck} />
-      <button onClick={handleSubmit}>Submit</button>
-    </div>
+      <input name='name' value={state.name} onChange={handleChange} />
+      <textarea
+        name='favoritePet'
+        value={state.favoritePet}
+        onChange={handleChange}
+      />
+      <input
+        name='rememberMe'
+        type='checkbox'
+        checked={state.rememberMe}
+        onChange={handleChange}
+      />
+      <button type='submit'>Submit</button>
+    </form>
   );
 }
 
